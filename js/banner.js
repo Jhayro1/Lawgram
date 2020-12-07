f3999671c7d2f57c601ef969023150869f3c26ba
Jhayro Jhordan Roncal Tacilla <jhayro2017@gmail.com>
2020-12-06 22:55:35 -0500

    Update banner.js

--- js/banner.js
+++ js/banner.js
@@ -11,5 +11,20 @@ $(document).ready(function () {
   alturaBanner();
 
   $(window).resize(alturaBanner);
+  
+  
+  
+ 
+  
+  $.ajaxSetup({ cache: true });
+  $.getScript('https://connect.facebook.net/en_US/sdk.js', function(){
+    FB.init({
+      appId: '{your-app-id}',
+      version: 'v2.7' // or v2.1, v2.2, v2.3, ...
+    });     
+    $('#loginbutton,#feedbutton').removeAttr('disabled');
+    FB.getLoginStatus(updateStatusCallback);
+  });
+
 
 });
